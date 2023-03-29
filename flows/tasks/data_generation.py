386b2cc5fea7099bff73dcd8d6a0fb9c075cad57
Tim-Oliver Buchholz <tim-oliver.buchholz@fmi.ch>
2023-03-29 13:17:30 +0200

    Shuffle image files.

--- flows/tasks/data_generation.py
+++ flows/tasks/data_generation.py
@@ -1,6 +1,7 @@
+import random
+from copy import copy
 from os.path import join
 
-import numpy as np
 from cpr.numpy.NumpyTarget import NumpyTarget
 from cpr.utilities.utilities import task_input_hash
 from n2v.internals.N2V_DataGenerator import N2V_DataGenerator
@@ -14,15 +15,15 @@ def extract_patches(
     patch_shape: list[int],
     output_dir: str,
 ):
-    np.random.seed(42)
-    idx = np.random.permutation(len(img_files))
+    img_files_shuffled = copy(img_files)
+    random.shuffle(img_files_shuffled)
     datagen = N2V_DataGenerator()
     split = int(min(max(len(img_files) * 0.1, 1), 500))
 
     x_val = NumpyTarget.from_path(join(output_dir, "x_val_2D.npy"))
     x_val.set_data(
         datagen.generate_patches_from_list(
-            img_files[idx][:split],
+            img_files_shuffled[:split],
             num_patches_per_img=num_patches_per_img,
             shape=patch_shape,
             augment=False,
@@ -32,7 +33,7 @@ def extract_patches(
     x = NumpyTarget.from_path(join(output_dir, "x_train_2D.npy"))
     x.set_data(
         datagen.generate_patches_from_list(
-            img_files[idx][split:],
+            img_files_shuffled[split:],
             num_patches_per_img=num_patches_per_img,
             shape=patch_shape,
             augment=True,
